b33d4e92754566ac420dd2df2f8a8fbf9a336d76
stonycat <zhuqiancg@163.com>
2021-02-19 15:56:50 +0800

    load json by js file
    update 2.19

---
+++ json/t1.js
@@ -0,0 +1,100 @@
+var context = {
+  "hotels": [
+    {
+      "name": "Hampton_by_Hilton_London_Waterloo",
+      "price": 99,
+      "img": "holder.js/250x250?theme=thumb",
+      "rating": 4.5,
+      "ratingNum": 870,
+      "reviews": [
+        {
+          "user_name": "bsandrs",
+          "user_link": "https://en.tripadvisor.com.hk/Profile/bsandrs",
+          "user_wrote_time": "Jan 2020",
+          "user_loc": "Portsmouth, United Kingdom",
+          "contriNum": "22,737 contributions",
+          "helpfulNum": "10,668 helpful votes",
+          "n_star": 50,
+          "title": "A pleasant stay",
+          "content": "We use this hotel for its convenience to London Waterloo station and its proximity to a number of buses which access central London and outer London. It is a well run and welcoming hotel with breakfast included. There are a number of room options providing double and twin beds. Recommended for short or long stays",
+          "time": " January 2020",
+          "helpful_vote": 10,
+          "polarity": 0.6710829436779022,
+          "keywords": "['hotel', 'breakfast', 'london', 'beds', 'welcoming']",
+          "categories": [
+              "facility"
+          ],
+          "userImg": "user-img.png"
+        },
+        {
+          "user_name": "Andrew N",
+          "user_link": "https://en.tripadvisor.com.hk/Profile/AndrewN424",
+          "user_wrote_time": "Jan 2020",
+          "user_loc": "Tadley, United Kingdom",
+          "contriNum": "820 contributions",
+          "helpfulNum": "285 helpful votes",
+          "n_star": 30,
+          "title": "Decent stay",
+          "content": "In order to ensure we got to our Winter Walk in time we stayed a single night in this hotel. Check in was rapid and friendly and the room functional enough, if somewhat small. Breakfast seemed well stocked with good variety, although sadly we didn’t have time to dwell.",
+          "time": " January 2020",
+          "helpful_vote": 0,
+          "polarity": 0.13431334495544436,
+          "keywords": "['breakfast', 'winter', 'night', 'hotel', 'stocked']",
+          "categories": [
+              "food"
+          ],
+          "userImg": "user-img.png"
+        }
+      ]
+    },
+    {
+      "name": "Holiday_Inn_Express_London_Victoria",
+      "price": 104,
+      "img": "holder.js/250x250?theme=thumb",
+      "rating": 3.5,
+      "ratingNum": 118,
+      "reviews": [
+          {
+              "user_name": "Joao S",
+              "user_link": "https://en.tripadvisor.com.hk/Profile/joaos2017",
+              "user_wrote_time": "Jan 2020",
+              "user_loc": "Lisbon, Portugal",
+              "contriNum": "62 contributions",
+              "helpfulNum": "23 helpful votes",
+              "n_star": 40,
+              "title": "Above the average in London",
+              "content": "Spent two nights in this hotel and I'm completely satisfied with my stay.  Wonderful staff. Thank's Maria (the receptionist) for your warm wellcome and thank's also to the night receptionist that help me to get a anniversary cake to my wife's birthday at 11pm. Thank you all. The breakfast is quite good (included) and the staff is very nice always with \"genuine smiles\" in their faces. The rooms are not very big but comfy and quiet enough for a good and restful night sleep. In summary, for all the reasons above, I definitly recommend this hotel to your next London trip if you go \"on a budget\". Ah! And its realy very close to the Pimlico tube station (5 min) or Victoria Station (10min).",
+              "time": " January 2020",
+              "helpful_vote": 0,
+              "polarity": 0.8572086095809937,
+              "keywords": "['cake', 'breakfast', 'receptionist', 'hotel', 'birthday']",
+              "categories": [
+                  "food"
+              ],
+              "userImg": "user-img.png"
+          },
+          {
+              "user_name": "Patcham99",
+              "user_link": "https://en.tripadvisor.com.hk/Profile/Patcham99",
+              "user_wrote_time": "Jan 2020",
+              "user_loc": "Brighton, United Kingdom",
+              "contriNum": "12 contributions",
+              "helpfulNum": "7 helpful votes",
+              "n_star": 40,
+              "title": "Long weekend",
+              "content": " Visited for three nights mid January. Lovely hotel, pleasant staff, clean and well maintained. Had double room on 3 floor. Very quiet no sound from Road or adjoining rooms. Visited in jan and room quite hot, so summer could be problem. No air con (due to building being listed) however fan on wall.  Bathroom good, shower good pressure and not huge but plenty of room. Beware London water is very ‘soft’ go easy on soap or you’ll be rinsing forever! Breakfast was good, plenty of selection and grab and go option. Coffee ok but bit weak.  Location is superb, there really is a pub on every corner! Pint of Doom Bar and G & T ranges from £9.75 - £11.00. The Warwick has cocktails for £5.50. Cafe Nero, Boots and small Tesco and Waitrose handy for odd bits, otherwise great local shops,",
+              "time": " January 2020",
+              "helpful_vote": 0,
+              "polarity": 0.05904622073285281,
+              "keywords": "['breakfast', 'summer', 'shower', 'soap', 'cocktails']",
+              "categories": [
+                  "food"
+              ],
+              "userImg": "user-img.png"
+          }
+        ]
+    }
+
+
+  ]
+}
