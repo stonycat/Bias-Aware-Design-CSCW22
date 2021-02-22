c2b0939e6faa7e1bbb3b016efc11351feba8c4bb
CinderD <63868789+CinderD@users.noreply.github.com>
2021-02-22 12:02:42 +0800

    Add files via upload

---
+++ hotels_data_upload/t1.js
@@ -0,0 +1,142 @@
+// import {Alhambra01} from './Alhambra_Hotel_95_4.0';
+document.write("<script type='text/javascript' src='Alhambra_Hotel_95_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Best_Western_Swiss_Cottage_Hotel_120_3.5.js'></script>");
+document.write("<script type='text/javascript' src='Camden_Enterprise_Hotel_106_4.5.js'></script>");
+document.write("<script type='text/javascript' src='Chelsea_Cloisters_79_3.0.js'></script>");
+document.write("<script type='text/javascript' src='Glenlyn_Hotel_91_4.5.js'></script>");
+document.write("<script type='text/javascript' src='Hampton_by_Hilton_London_Waterloo_99_4.5.js'></script>");
+document.write("<script type='text/javascript' src='Henley_House_Hotel_107_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Holiday_Inn_Express_London_Hammersmith_99_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Holiday_Inn_Express_London_Victoria_104_3.5.js'></script>");
+document.write("<script type='text/javascript' src='Holiday_Inn_London_West_110_4.5.js'></script>");
+document.write("<script type='text/javascript' src='Ibis_London_Blackfriars_Hotel_90_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Ibis_London_Earls_Court_108_3.5.js'></script>");
+document.write("<script type='text/javascript' src='Ibis_Styles_London_Southwark_99_4.0.js'></script>");
+document.write("<script type='text/javascript' src='La_Suite_West_Hyde_Park_102_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Lancaster_Gate_Hotel_118_3.5.js'></script>");
+document.write("<script type='text/javascript' src='London_House_Hotel_108_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Martel_Guest_House_90_4.5.js'></script>");
+document.write("<script type='text/javascript' src='Mayflower_Hotel_90_3.0.js'></script>");
+document.write("<script type='text/javascript' src='OYO_Huttons_Hotel_82_3.0.js'></script>");
+document.write("<script type='text/javascript' src='Point_A_Hotel_London_Shoreditch_102_4.5.js'></script>");
+document.write("<script type='text/javascript' src='Prince_Regent_Hotel_Excel_London_103_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Residence_Inn_Marriott_London_Bridge_103_4.5.js'></script>");
+document.write("<script type='text/javascript' src='St_Giles_London_120_3.5.js'></script>");
+document.write("<script type='text/javascript' src='The_29_London_FKA_Airways_Hotel_Victoria_79_3.0.js'></script>");
+document.write("<script type='text/javascript' src='The_Captain_Cook_103_4.5.js'></script>");
+document.write("<script type='text/javascript' src='The_Royal_Foundation_of_St_Katharine_107_4.5.js'></script>");
+document.write("<script type='text/javascript' src='The_W14_Kensington_79_4.0.js'></script>");
+document.write("<script type='text/javascript' src='The_Westbourne_Hyde_Park_116_4.5.js'></script>");
+document.write("<script type='text/javascript' src='Thistle_City_Barbican_Shoreditch_103_3.0.js'></script>");
+document.write("<script type='text/javascript' src='Travelodge_London_Central_City_Road_99_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Travelodge_London_Docklands_84_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Travelodge_London_Excel_84_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Travelodge_London_Greenwich_High_Road_99_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Travelodge_London_Vauxhall_Hotel_95_4.0.js'></script>");
+document.write("<script type='text/javascript' src='Victor_Hotel_London_Victoria_101_3.5.js'></script>");
+
+// var Martel01 = {"name": "Martel_Guest_House", "price": "$90", "img": "holder.js/250x250?theme=thumb", "rating": "4.5", "ratingNum": 7, "reviews": [{"user_name": "Neildavidson", "user_wrote_time": "Sep 2019", "contriNum": "93 contributions", "helpfulNum": "81 helpful votes", "currentRating": "1.0", "title": "Misrepresented", "content": "The picture gave the impression of a spacious bedroom with a large double bed . In fact the room was tiny with the smallest double possible crammed in and a poky bathroom that was an invitation to slip and fall .  The breakfast was totally tasteless and meagre . Will not be visiting again .", "time": " September 2019", "helpful_vote": 2, "polarity": -0.592, "keywords": "['bed', 'bedroom', 'breakfast', 'tasteless', 'bathroom']", "categories": ["facility"], "userImg": "user-img.png"}, {"user_name": "foluke2016", "user_wrote_time": "Aug 2019", "contriNum": "70 contributions", "helpfulNum": "26 helpful votes", "currentRating": "4.0", "title": "Looks can be deceptive!", "content": "Even though the guest house does not have curb appeal, the interior is surprisingly good. Breakfast is included I am not really a breakfast person but did make a point of duty to visit the breakfast room, which is compact with outdoor sitting on the small patio in the garden Had a single room, small but every inch well used, with ample space for a TV, cupboard, fridge, safe, coffee/tea station and coffee machine and A/C. The room is tastefully decorated in black.  The bathroom or rather a wet cell was clean but dated, the grouting is in need of repair. Comfortable single bed, which was made every day, regular change of towels and a super-fast Wifi. View from the window is the neighbour's wall! Location is very good a 5 minutes walk to the Underground and bus station. The owner is pleasant and friendly.", "time": " August 2019", "helpful_vote": 1, "polarity": 0.416, "keywords": "['fridge', 'breakfast', 'tea', 'bed', 'bathroom']", "categories": ["facility"], "userImg": "user-img.png"}, {"user_name": "julianD5510IX", "user_wrote_time": "Aug 2019", "contriNum": "4 contributions", "helpfulNum": "1 helpful vote", "currentRating": "4.0", "title": "A perfect stay", "content": "I was in London for 2 nights for a business trip and stayed at Martel. Just what I needed! A small homely and clean place in the Jewish neighborhood close to synagogues and Kosher restaurants. The proprietor was very friendly and helpful and the rooms were quiet. A pity their breakfast (which looked very good) was not Kosher. The bathroom was a little too small with a very noisy hot water pump system. On the whole I would recommend and return there myself. Very good value for money.", "time": " July 2019", "helpful_vote": 1, "polarity": 0.281, "keywords": "['kosher', 'breakfast', 'restaurants', 'synagogues', 'london']", "categories": ["food"], "userImg": "user-img.png"}, {"user_name": "MonLantau", "user_wrote_time": "Jul 2019", "contriNum": "46 contributions", "helpfulNum": "35 helpful votes", "currentRating": "5.0", "title": "Even better after refurbishment ", "content": "We had a very pleasant stay at The Martel before and always planned to return. This time around it was even better as the place has been upgraded throughout. The new patio off the dining room is perfect for breakfast (we were lucky with the weather), the food selection is good and best of all, the coffee machines in the room churn out amazing coffee. Mr. Martel is always eager to help and ensure things run seamlessly. Everything is spotlessly clean and the place is very convenient for transport and shops, banks and restaurants. ", "time": " July 2019", "helpful_vote": 1, "polarity": 0.946, "keywords": "['breakfast', 'coffee', 'restaurants', 'upgraded', 'dining']", "categories": ["food"], "userImg": "user-img.png"}, {"user_name": "RodneyaB", "user_wrote_time": "Oct 2018", "contriNum": "1 contribution", "helpfulNum": "1 helpful vote", "currentRating": "5.0", "title": "Hidden Jewel", "content": "I am not the B&B type and would never have thought of booking here. It was last minute and my PA booked me at the Martel. Wow was I surprised? The place is spotless and the owner/host \"Phil\" is an excellent host.  The breakfast is first class. He does the cooking himself and it's as good as any 4-star hotel in the city.  Great Job Phil. I will stay again whenever my travels bring me to London. This is a winner.", "time": " October 2018", "helpful_vote": 1, "polarity": 0.409, "keywords": "['breakfast', 'cooking', 'hotel', 'host', 'owner']", "categories": ["food"], "userImg": "user-img.png"}, {"user_name": "Steinar F", "user_wrote_time": "May 2018", "contriNum": "40 contributions", "helpfulNum": "19 helpful votes", "currentRating": "5.0", "title": "A peaceful place to stay", "content": "We'we been here before - and we'll most certainly come back next year (or earlier)! Last year we stayed down town (near Paddington Stn.) because some of our family members wanted to, but this year both economy and experience from earlier stays made the choice easy - Golders Green and Martel Guest House! Thank you Phil, for a nice 4 nights stay at your b&b. Here we find clean quiet rooms, nice beds, great breakfast and a very friendly staff!", "time": " May 2018", "helpful_vote": 2, "polarity": 0.324, "keywords": "['breakfast', 'nights', 'guest', 'family', 'economy']", "categories": ["food"], "userImg": "user-img.png"}, {"user_name": "Rahul D", "user_wrote_time": "Apr 2018", "contriNum": "6 contributions", "helpfulNum": "3 helpful votes", "currentRating": "5.0", "title": "Cozy and comfortable with a charming owner", "content": "We stayed at the Martel guest house during our recent visit to London and was pleased with the property and particularly the owner Phil Martel. Phil is an outright gentleman who will do everything possible to make your stay comfortable. We booked the family room for our family of 2 adults and 2 kids. The room and the bathroom was neat and clean and opened to a pleasing backyard. The bathroom was small particularly the shower stall but I guess, we are a bit spoilt in the US. Note that the water heater in the shower (at least in our room) is tankless and can take a bit of time to heat up. We were fine with the amenities and got adjusted pretty quickly.  The guest house is in a quiet neighborhood in Golders Green and easily accessible from the tube (northern line) and bus station.", "time": " March 2018", "helpful_vote": 2, "polarity": 0.21, "keywords": "['bathroom', 'shower', 'london', 'house', 'owner']", "categories": ["facility"], "userImg": "user-img.png"}]}
+var context = {
+  "hotels": [Alhambra01,Best01,Camden01,Chelsea01,Glenyln01,Hampton01,Henley01,Holiday01,Holiday02,Holiday03,Ibis01,Ibis02,Ibis03,La01,Lancaster01,London01,Martel01,Mayflower01,OYO01,PointA01,Prince01,Residence01,St01,The2901,TheCaptain01,TheRoyal01,TheW1401,TheWestbourne01,Thistle01,Travelodge01,Travelodge02,Travelodge03,Travelodge04,Travelodge05,Victor01]
+}
+// Chelsea01，Ibis03,London01
+// var context = {
+//   "hotels": [
+//     {
+//       "name": "Hampton_by_Hilton_London_Waterloo",
+//       "price": 99,
+//       "img": "holder.js/250x250?theme=thumb",
+//       "rating": 4.5,
+//       "ratingNum": 870,
+//       "reviews": [
+//         {
+//           "user_name": "bsandrs",
+//           "user_link": "https://en.tripadvisor.com.hk/Profile/bsandrs",
+//           "user_wrote_time": "Jan 2020",
+//           "user_loc": "Portsmouth, United Kingdom",
+//           "contriNum": "22,737 contributions",
+//           "helpfulNum": "10,668 helpful votes",
+//           "n_star": 50,
+//           "title": "A pleasant stay",
+//           "content": "We use this hotel for its convenience to London Waterloo station and its proximity to a number of buses which access central London and outer London. It is a well run and welcoming hotel with breakfast included. There are a number of room options providing double and twin beds. Recommended for short or long stays",
+//           "time": " January 2020",
+//           "helpful_vote": 10,
+//           "polarity": 0.6710829436779022,
+//           "keywords": "['hotel', 'breakfast', 'london', 'beds', 'welcoming']",
+//           "categories": [
+//               "facility"
+//           ],
+//           "userImg": "user-img.png"
+//         },
+//         {
+//           "user_name": "Andrew N",
+//           "user_link": "https://en.tripadvisor.com.hk/Profile/AndrewN424",
+//           "user_wrote_time": "Jan 2020",
+//           "user_loc": "Tadley, United Kingdom",
+//           "contriNum": "820 contributions",
+//           "helpfulNum": "285 helpful votes",
+//           "n_star": 30,
+//           "title": "Decent stay",
+//           "content": "In order to ensure we got to our Winter Walk in time we stayed a single night in this hotel. Check in was rapid and friendly and the room functional enough, if somewhat small. Breakfast seemed well stocked with good variety, although sadly we didn’t have time to dwell.",
+//           "time": " January 2020",
+//           "helpful_vote": 0,
+//           "polarity": 0.13431334495544436,
+//           "keywords": "['breakfast', 'winter', 'night', 'hotel', 'stocked']",
+//           "categories": [
+//               "food"
+//           ],
+//           "userImg": "user-img.png"
+//         }
+//       ]
+//     },
+//     {
+//       "name": "Holiday_Inn_Express_London_Victoria",
+//       "price": 104,
+//       "img": "holder.js/250x250?theme=thumb",
+//       "rating": 3.5,
+//       "ratingNum": 118,
+//       "reviews": [
+//           {
+//               "user_name": "Joao S",
+//               "user_link": "https://en.tripadvisor.com.hk/Profile/joaos2017",
+//               "user_wrote_time": "Jan 2020",
+//               "user_loc": "Lisbon, Portugal",
+//               "contriNum": "62 contributions",
+//               "helpfulNum": "23 helpful votes",
+//               "n_star": 40,
+//               "title": "Above the average in London",
+//               "content": "Spent two nights in this hotel and I'm completely satisfied with my stay.  Wonderful staff. Thank's Maria (the receptionist) for your warm wellcome and thank's also to the night receptionist that help me to get a anniversary cake to my wife's birthday at 11pm. Thank you all. The breakfast is quite good (included) and the staff is very nice always with \"genuine smiles\" in their faces. The rooms are not very big but comfy and quiet enough for a good and restful night sleep. In summary, for all the reasons above, I definitly recommend this hotel to your next London trip if you go \"on a budget\". Ah! And its realy very close to the Pimlico tube station (5 min) or Victoria Station (10min).",
+//               "time": " January 2020",
+//               "helpful_vote": 0,
+//               "polarity": 0.8572086095809937,
+//               "keywords": "['cake', 'breakfast', 'receptionist', 'hotel', 'birthday']",
+//               "categories": [
+//                   "food"
+//               ],
+//               "userImg": "user-img.png"
+//           },
+//           {
+//               "user_name": "Patcham99",
+//               "user_link": "https://en.tripadvisor.com.hk/Profile/Patcham99",
+//               "user_wrote_time": "Jan 2020",
+//               "user_loc": "Brighton, United Kingdom",
+//               "contriNum": "12 contributions",
+//               "helpfulNum": "7 helpful votes",
+//               "n_star": 40,
+//               "title": "Long weekend",
+//               "content": " Visited for three nights mid January. Lovely hotel, pleasant staff, clean and well maintained. Had double room on 3 floor. Very quiet no sound from Road or adjoining rooms. Visited in jan and room quite hot, so summer could be problem. No air con (due to building being listed) however fan on wall.  Bathroom good, shower good pressure and not huge but plenty of room. Beware London water is very ‘soft’ go easy on soap or you’ll be rinsing forever! Breakfast was good, plenty of selection and grab and go option. Coffee ok but bit weak.  Location is superb, there really is a pub on every corner! Pint of Doom Bar and G & T ranges from £9.75 - £11.00. The Warwick has cocktails for £5.50. Cafe Nero, Boots and small Tesco and Waitrose handy for odd bits, otherwise great local shops,",
+//               "time": " January 2020",
+//               "helpful_vote": 0,
+//               "polarity": 0.05904622073285281,
+//               "keywords": "['breakfast', 'summer', 'shower', 'soap', 'cocktails']",
+//               "categories": [
+//                   "food"
+//               ],
+//               "userImg": "user-img.png"
+//           }
+//         ]
+//     }
+
+
+//   ]
+// }
